0a8a9faf979b4f8a5799ae2cadb422e111e55cb3
望月祐汰 <mczk9402@gmail.com>
2021-12-28 21:13:51 +0900

    検索機能追加

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,34 @@
-import { useEffect, useState, VFC } from "react";
-import styles from "./styles/images.module.scss";
+import { useEffect, useState, VFC, FormEvent } from 'react';
+import styles from './styles/images.module.scss';
 
 export const Hoge: VFC<{ foo: string }> = (props) => {
   const [images, setImages] = useState<string[]>([]);
-  const [breed, setBreed] = useState("shiba");
+  const [breed, setBreed] = useState('shiba');
+  const [formText, setFormText] = useState<string>('');
+
+  const fetchImages = async (breed: string): Promise<string[]> => {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/12`);
+    const data = await response.json();
+    console.log('fetchImages');
+    return data.message;
+  };
+
+  const a = async () => {
+    const image = await fetchImages(breed);
+    console.log('a');
+    setImages(image);
+  };
 
   useEffect(() => {
-    async function fetchImages(breed: string): Promise<string[]> {
-      const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/12`);
-      const data = await response.json();
-      return data.message;
-    }
-
-    const a = async () => {
-      const image = await fetchImages(breed);
-      setImages(image);
-    };
-
     a();
   }, [breed]);
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const image = await fetchImages(formText);
+    setImages(image);
+    setFormText('');
+  };
 
   return (
     <div>
@@ -34,6 +44,10 @@ export const Hoge: VFC<{ foo: string }> = (props) => {
         <option value="shiba">shiba</option>
         <option value="akita">akita</option>
       </select>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <input value={formText} onChange={(e) => setFormText(e.target.value)} />
+        <button type="submit">検索</button>
+      </form>
       <ul>
         {images.map((src) => (
           <li key={src}>
