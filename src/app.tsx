ac201a42ca6a0da54c04a8d4421b27e4c12b6c8b
望月祐汰 <mczk9402@gmail.com>
2021-12-28 23:25:26 +0900

    ⚡️ 検索を即時反映されるように、 apiでリストを取得、optionに反映

--- src/app.tsx
+++ src/app.tsx
@@ -5,49 +5,82 @@ export const Hoge: VFC<{ foo: string }> = (props) => {
   const [images, setImages] = useState<string[]>([]);
   const [breed, setBreed] = useState('shiba');
   const [formText, setFormText] = useState<string>('');
-
-  const fetchImages = async (breed: string): Promise<string[]> => {
-    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/12`);
-    const data = await response.json();
-    console.log('fetchImages');
-    return data.message;
-  };
-
-  const a = async () => {
-    const image = await fetchImages(breed);
-    console.log('a');
-    setImages(image);
-  };
+  const [breedList, setBreedList] = useState<string[]>([]);
 
   useEffect(() => {
+    const fetchImages = async (breed: string): Promise<string[]> => {
+      const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/12`);
+      const data = await response.json();
+      console.log('fetchImages');
+      return data.message;
+    };
+
+    const a = async () => {
+      const image = await fetchImages(breed);
+      console.log(image);
+      setImages(image);
+    };
+
     a();
   }, [breed]);
 
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const image = await fetchImages(formText);
-    setImages(image);
-    setFormText('');
-  };
+  useEffect(() => {
+    const search = async () => {
+      await fetch(`https://dog.ceo/api/breed/${formText}/images/random/12`)
+        .then(async (response) => {
+          if (!response.ok) return console.error('サーバーエラー');
+          setBreed(formText);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    search();
+  }, [formText]);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      await fetch('https://dog.ceo/api/breeds/list/all').then(async (res) => {
+        const data = await res.json();
+        setBreedList(Object.keys(data.message));
+      });
+    };
+    fetchList();
+  }, []);
 
   return (
     <div>
-      <h1 className={styles.foo}>{props.foo}</h1>
+      <h1>{props.foo}</h1>
       <select
         name=""
         id=""
         value={breed}
         onChange={(event) => {
           setBreed(event.target.value);
+          setFormText(event.target.value);
         }}
       >
-        <option value="shiba">shiba</option>
-        <option value="akita">akita</option>
+        {breedList.map((breed, index) => (
+          <option value={breed} key={index}>
+            {breed}
+          </option>
+        ))}
       </select>
-      <form onSubmit={(e) => onSubmit(e)}>
-        <input value={formText} onChange={(e) => setFormText(e.target.value)} />
-        <button type="submit">検索</button>
-      </form>
+      <input value={formText} onChange={(e) => setFormText(e.target.value)} />
+      {/* <div className="buttonWrapper">
+        {breedList.map((breed, index) => (
+          <button
+            onClick={() => {
+              setBreed(breed);
+              setFormText(breed);
+            }}
+            key={index}
+          >
+            {breed}
+          </button>
+        ))}
+      </div> */}
       <ul>
         {images.map((src) => (
           <li key={src}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { render } from "react-dom";
-import { Hoge } from "./app";
-import "./styles/style.scss";
+import { render } from 'react-dom';
+import { Hoge } from './app';
+import './styles/style.scss';
 
-render(<Hoge foo={"asd"} />, document.querySelector("#root"));
+render(<Hoge foo={'犬画像ギャラリー'} />, document.querySelector('#root'));
